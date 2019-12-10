f09a90cfd58fcb857c0f1bdcee4df2f17ee3c27b
tseijp <yousei_san@icloud.com>
2019-12-10 12:22:47 +0000

    from AWS

--- tseijp/settings/prod.py
+++ tseijp/settings/prod.py
@@ -3,11 +3,7 @@ from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
 SECRET_KEY='**************************************************'
-<<<<<<< HEAD
-DEBUG = False#True
-=======
 DEBUG         = False
->>>>>>> 051199563d432da716ed2fba74ea893199f811d3
 ALLOWED_HOSTS = ['tsei.jp', '3.134.52.211', '3.134.52.211:8000']
 
 # Database
