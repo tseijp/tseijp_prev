044bf0ceefd40d2d611dcc1b940d51a66732a7c4
MY\yousei takei <yousei_san@icloud.com>
2019-12-20 00:23:19 +0900

    add ret from WIN

--- app_note/urls.py
+++ app_note/urls.py
@@ -5,7 +5,7 @@ from app_note.views import *
 from app_note.ajaxs import *
 
 urlpatterns = [
-    path('test'              , qiita_init              , name='note_test'     ),
+    #path('test'              , qiita_init              , name='note_test'     ),
     #path('drop'              , drop_all              , name='note_drop'     ),
     path(''                  , NoteHomeView.as_view()  , name='note'          ),
     #path('<int:pk>'          , NoteDetailView.as_view(), name='note_detail'   ),
@@ -16,8 +16,6 @@ urlpatterns = [
 
     #path('edit/<int:pk>'     , NoteEditView.as_view()  , name='note_edit'     ),
     path('delete/<int:pk>'   , NoteDeleteView.as_view(), name='note_delete'   ),
-#    path('posted/raw/<int:pk>'   , NotePostedRawView.as_view()   , name='note_posted_raw'  ),
-#    path('update/raw/<int:pk>'   , NoteUpdateRawView.as_view()   , name='note_update_raw'  ),
     path('ja/<int:pk>' , NoteJaView.as_view() , name='note_ja'),
     path('en/<int:pk>' , NoteEnView.as_view() , name='note_en'),
 ]
