6f49d9c2bafe1cec89e4e5727d4e33089bd18597
MY\yousei takei <yousei_san@icloud.com>
2020-01-12 07:50:50 +0900

    add cat iframe in server error 500

--- app_user/views.py
+++ app_user/views.py
@@ -1,4 +1,5 @@
 ### Python
+import random
 from datetime import datetime
 ### Django
 from django.urls import reverse_lazy
@@ -22,14 +23,30 @@ from app_user.models import *
 from app_user.values import *
 #from app_user.user_init import *
 
-
 def home(request):  return render(request, 'index/index.html', {"content":get_index_content()})
 def test(request):  return render(request, 'user_test.html')
 def code1(request): return render(request, 'code/tree.html')
 
+cat_iframes = [
+    '<iframe src="https://giphy.com/embed/JIX9t2j0ZTN9S" width="480" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/JIX9t2j0ZTN9S">via GIPHY</a></p>',
+    '<iframe src="https://giphy.com/embed/VbnUQpnihPSIgIXuZv" width="384" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/computer-cat-wearing-glasses-VbnUQpnihPSIgIXuZv">via GIPHY</a></p>',
+    '<iframe src="https://giphy.com/embed/jRlP4zbERYW5HoCLvX" width="480" height="270" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/jRlP4zbERYW5HoCLvX">via GIPHY</a></p>',
+    '<iframe src="https://giphy.com/embed/33OrjzUFwkwEg" width="435" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/cat-33OrjzUFwkwEg">via GIPHY</a></p>',
+    '<iframe src="https://giphy.com/embed/UZzuqeBeRcD3W" width="480" height="346" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/dance-dancing-cat-UZzuqeBeRcD3W">via GIPHY</a></p>',
+    '<iframe src="https://giphy.com/embed/6bAZXey5wNzBC" width="480" height="336" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/cat-kitty-funny-6bAZXey5wNzBC">via GIPHY</a></p>',
+    '<iframe src="https://giphy.com/embed/uZxa6mNaSihVu" width="480" height="272" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/cat-funny-uZxa6mNaSihVu">via GIPHY</a></p>',
+    '<iframe src="https://giphy.com/embed/CCK9EUDEIe2sw" width="480" height="360" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/cat-everybody-CCK9EUDEIe2sw">via GIPHY</a></p>',
+]
+
+def get_cat_iframe():
+    return random.choices(cat_iframess)[0]
+
 @requires_csrf_token
 def my_customized_server_error(request, template_name='500.html'):
-    return HttpResponseServerError('<h1>Server Error (500)!</h1>t1810394@edu.cc.uec.ac.jp')
+    message  = '<h1>Server Error (500)!</h1>'
+    message += 't1810394@edu.cc.uec.ac.jp'
+    message += get_cat_iframe()
+    return HttpResponseServerError(message)
 
 class SignupView(CreateView):
     form_class    = UserCreationForm
