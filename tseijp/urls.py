51b44f5492dd3ab4383c9277d8c3ed77b90e44f0
tseijp <t1810394@edu.cc.uec.ac.jp>
2021-04-20 11:17:53 +0900

    :sparkles:feat: monorepo

--- tseijp/urls.py
+++ tseijp/urls.py
@@ -4,32 +4,32 @@
 import os
 import os.path as osp
 from django.conf import settings
-from django.urls import include, path, re_path
+from django.urls import path, re_path
 from django.contrib import admin
 # from rest_framework import routers
 from django.conf.urls.static import static
 from django.views.generic.base import TemplateView
 from rest_framework.authtoken.views import obtain_auth_token
 # """"""""""""""" MY CREATED """"""""""""""" #
-# from note.views import UserViewSet, NoteViewSet
 from .utils import my_customized_server_error
+from note.urls import urlpatterns as note_urlpatterns
 from django.conf.urls import handler500
-
 handler500 = my_customized_server_error
+
+static_dir = 'tseijp/static/'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', obtain_auth_token),
-    # path('api/', include(router.urls)),
-    path('api/', include('note.urls')),
+    # *note_urlpatterns,
     *static(settings.STATIC_URL, document_root=settings.STATIC_ROOT),
 ]
 
-submodules = [s for s in os.listdir() if osp.isfile("tseijp/static/%s/index.html" % (s))]
+submodules = [s for s in os.listdir() if osp.isfile("%s%s/index.html" % (static_dir, s))]
 submodules = sorted(submodules, key=lambda v: v == "core")
 
 for name in submodules:
     route = name if name != "core" else ""
-    temp = 'tseijp/static/%s/index.html' % name
+    temp = '%s%s/index.html' % (static_dir, name)
     view = TemplateView.as_view(template_name=temp)
     urlpatterns += [re_path(route, view, name=name)]
